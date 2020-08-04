b85511e1b2b4af1c52decd508cc78dd9d9cff856
wani_khawar <wanikhawar@gmail.com>
2020-08-04 19:26:17 +0530

    Added function_plot.m

---
+++ plotting/function_plot.m
@@ -0,0 +1,11 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Script: Plotting Functions
+% Author: L0gic41
+% Date  : 04/08/2020
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+syms x y
+y = x^2
+fplot(y)
+xlim([0,10])
+ylim([0,100])
+grid on
